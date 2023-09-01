d94dba4b0acbcb88fdd8db4504beab2514f9e39a
Pandu Setia Darmawan <pandusetiadarmawan01@gmail.com>
2023-09-01 19:19:23 +0700

    membuat home portfolio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// toggle
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+// hamburger menu klik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
